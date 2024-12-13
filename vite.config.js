36fc2128b03ee216313deb65c2aafc024cdac372
alejandro.hpestana <alejandro.hpestana@bitboxonline.com>
2024-12-13 11:16:10 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/galleryzk/', // Ruta base para GitHub Pages
+  base: '/galleryZK/', // Ruta base para GitHub Pages
   build: {
     outDir: 'docs', // Cambia 'dist' por el nombre que desees, en este caso 'public'
   },
